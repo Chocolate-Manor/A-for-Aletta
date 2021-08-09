3a8025a1ac3b2a8b635faf480f529a6f26aa1052
Haocheng Yang <H.Yang-15@student.tudelft.nl>
2021-08-09 23:39:46 +0200

    add animation trigger (one idea)
    next, try timeline! See if you can integrate triggers. This would make it cutscene playable. Damn.

--- Assets/Tiled Maps/Victorian Objects.tsx
+++ Assets/Tiled Maps/Victorian Objects.tsx
@@ -2,33 +2,36 @@
 <tileset version="1.5" tiledversion="1.7.1" name="Victorian Objects" tilewidth="96" tileheight="54" tilecount="11" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="44" height="54" source="../Tiled Maps/Tiled Objects/double bookshelf.png"/>
+  <image width="44" height="54" source="Tiled Objects/double bookshelf.png"/>
  </tile>
  <tile id="1">
-  <image width="21" height="8" source="../Tiled Maps/Tiled Objects/photo collection.png"/>
+  <image width="21" height="8" source="Tiled Objects/photo collection.png"/>
  </tile>
  <tile id="2">
-  <image width="66" height="34" source="../Tiled Maps/Tiled Objects/reception desk.png"/>
+  <image width="66" height="34" source="Tiled Objects/reception desk.png"/>
  </tile>
  <tile id="3">
-  <image width="35" height="53" source="../Tiled Maps/Tiled Objects/single bookshelf.png"/>
+  <image width="35" height="53" source="Tiled Objects/single bookshelf.png"/>
  </tile>
  <tile id="8">
-  <image width="96" height="48" source="../Tiled Maps/Tiled Objects/carpet.png"/>
+  <image width="96" height="48" source="Tiled Objects/carpet.png"/>
  </tile>
  <tile id="9">
-  <image width="96" height="32" source="../Tiled Maps/Tiled Objects/short carpet.png"/>
+  <image width="96" height="32" source="Tiled Objects/short carpet.png"/>
  </tile>
  <tile id="15">
-  <image width="32" height="48" source="../Tiled Maps/Characters/Briella colored.png"/>
+  <image width="32" height="48" source="Characters/Briella colored.png"/>
  </tile>
  <tile id="18">
-  <image width="33" height="48" source="../Tiled Maps/Characters/Office girl 1-back.png"/>
+  <image width="33" height="48" source="Characters/Office girl 1-back.png"/>
  </tile>
  <tile id="19">
-  <image width="33" height="48" source="../Tiled Maps/Characters/Floris back.png"/>
+  <image width="33" height="48" source="Characters/Floris back.png"/>
  </tile>
  <tile id="20">
-  <image width="33" height="48" source="../Tiled Maps/Characters/Office girl 1.png"/>
+  <image width="33" height="48" source="Characters/Office girl 1.png"/>
+ </tile>
+ <tile id="21">
+  <image width="33" height="48" source="Characters/Floris front.png"/>
  </tile>
 </tileset>
