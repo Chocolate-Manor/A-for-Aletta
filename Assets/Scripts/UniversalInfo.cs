c15f1bb1f1196bb58a9b20912d1c2f50835b0b54
Haocheng <H.Yang-15@student.tudelft.nl>
2021-11-09 10:30:23 +0100

    Setting up save system, add title pic

--- Assets/Scripts/UniversalInfo.cs
+++ Assets/Scripts/UniversalInfo.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 public static class UniversalInfo
 {
@@ -8,4 +9,21 @@ public static class UniversalInfo
     //public static delegate when next scene happens
     public delegate void nextConvDel(int curConvIndex);
     public static nextConvDel nextSceneEvent = delegate {};
+
+    /// <summary>
+    /// Increment the current convIndex and save it inside playerprefs.
+    /// </summary>
+    public static void Increment_ConvIndex_And_Save()
+    {
+        curConvIndex++;
+        PlayerPrefs.SetInt("curConvIndex", curConvIndex);
+    }
+    
+    /// <summary>
+    /// Load the current ConvIndex to see 
+    /// </summary>
+    public static void Load_ConvIndex()
+    {
+        curConvIndex = PlayerPrefs.GetInt("curConvIndex");
+    }
 }
