80a5b6c96ae48e42d1f157ac48a2f38c635a5efe
howchen217 <59067683+howchen217@users.noreply.github.com>
2022-04-26 17:26:05 +0200

    make steam overlay work

--- Assets/Scripts/Steam/SteamManager.cs
+++ Assets/Scripts/Steam/SteamManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SteamManager : MonoBehaviour
+public class SteamManager : SingletonMonoBehavior<SteamManager>
 {
     private void Awake()
     {
